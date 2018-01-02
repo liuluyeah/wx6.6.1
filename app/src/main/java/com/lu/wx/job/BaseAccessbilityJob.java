1b324490ba22147f5276cc619102dfd416e94050
liulu <liul@ruyi.ai>
2018-01-02 15:08:43 +0800

    update

---
+++ app/src/main/java/com/lu/wx/job/BaseAccessbilityJob.java
@@ -0,0 +1,35 @@
+package com.lu.wx.job;
+
+import android.content.Context;
+
+import com.lu.wx.Config;
+import com.lu.wx.QiangHongBaoService;
+
+/**
+ * <p>Created 16/1/16 上午12:38.</p>
+ * <p><a href="mailto:codeboy2013@gmail.com">Email:codeboy2013@gmail.com</a></p>
+ * <p><a href="http://www.happycodeboy.com">LeonLee Blog</a></p>
+ *
+ * @author LeonLee
+ */
+public abstract class BaseAccessbilityJob implements AccessbilityJob {
+
+    private QiangHongBaoService service;
+
+    @Override
+    public void onCreateJob(QiangHongBaoService service) {
+        this.service = service;
+    }
+
+    public Context getContext() {
+        return service.getApplicationContext();
+    }
+
+    public Config getConfig() {
+        return service.getConfig();
+    }
+
+    public QiangHongBaoService getService() {
+        return service;
+    }
+}
